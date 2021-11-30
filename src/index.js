a90c20f03a7e51d84b81f779336ebf2631b4ad12
Victor Wright <victor.wright@outlook.de>
2021-11-30 11:47:33 +0100

    trying gh-pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {  BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      {/* <HashRouter> */}
+      <App />
+      {/* </HashRouter> */}
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
